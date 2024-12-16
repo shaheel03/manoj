f39cbe0be1bbf406293dd173868d9d6be80885f0
shaheelkumar <shaheelkumar@gmail.com>
2024-12-16 22:28:38 +0530

    first commit

---
+++ Resource/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  subscription_id = "db3755c9-aab3-486b-9a5c-a09c0e660ea7"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+
+
+    name="jaggy-rg"
+    location = "west us"
+  
+}
